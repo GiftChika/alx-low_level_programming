afcfa72e593c91c23d08015c3cfa811b4cb401f6
GiftChika <giftchika222@gmail.com>
2022-10-13 12:07:58 -0700

    just a commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Prints a text
  *
- * Return : Always 0
+ * Return : Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
