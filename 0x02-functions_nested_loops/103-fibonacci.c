0f8ef2ae282ac53856e4d6cfb90045bc1e028722
GiftChika <giftchika222@gmail.com>
2022-10-19 14:34:04 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of even-valued Fibonacci numbers
+ * number should not exceed 4,000,000.
+ * number should start with 1 and 2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long first_num = 0, second_num = 1;
+	unsigned long sum;
+	float total_num;
+
+	while (1)
+	{
+		sum = first_num + second_num;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total_num += sum;
+
+		first_num = second_num;
+		second_num = sum;
+	}
+	printf("%.0f\n", total_num);
+
+	return (0);
+}
