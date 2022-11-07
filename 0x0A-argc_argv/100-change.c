095eed6a24a9258e3033ae54d478df274e64e84d
GiftChika <giftchika222@gmail.com>
2022-11-07 08:33:24 -0800

    prints the minimum number of coins to make change for an amount of money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the minimum number of coins to make change
+ * @argc: argument count
+ * @argv: string vector of argc
+ *
+ * Did not use atoi
+ * because didin't give any flags when there was a conv error
+ *
+ * Return: change, error and 1 if is arg < 2, 0 if no arg provided
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int i;
+	int amount, change;
+	int cents[] = {25, 10, 5, 2, 1};
+	char *a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	amount = strtol(argv[1], &a, 10);
+	change = 0;
+
+	if (!*a)
+	{
+		while (amount > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (amount >= cents[i])
+			{
+				change += amount / cents[i];
+				amount = amount % cents[i];
+			}
+		}
+	}
+	if (amount == 1)
+		change++;
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	printf("%d\n", change);
+	return (0);
+}
